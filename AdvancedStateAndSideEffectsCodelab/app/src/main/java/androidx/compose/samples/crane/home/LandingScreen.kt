fbd48237f90064566556c0198dc97d4b5758b4a7
Oseong Kwon <oxojjjj@gmail.com>
2022-04-18 16:05:42 +0900

    side-effect done

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/home/LandingScreen.kt
@@ -20,10 +20,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 
 private const val SplashWaitTime: Long = 2000
 
@@ -32,6 +37,28 @@ fun LandingScreen(modifier: Modifier = Modifier, onTimeout: () -> Unit) {
     Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         // TODO Codelab: LaunchedEffect and rememberUpdatedState step
         // TODO: Make LandingScreen disappear after loading data
+
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeout by rememberUpdatedState(onTimeout)
+
+        // Start a side effect to load things in the background
+        // and call onTimeout() when finished.
+        // Passing onTimeout as a parameter to LaunchedEffect
+        LaunchedEffect(true) {
+            delay(SplashWaitTime) // Simulates loading things
+
+            // Create an effect that matches the lifecycle of LandingScreen.
+            // If LandingScreen recomposes or onTimeout changes,
+            // the delay shouldn't start again.
+            currentOnTimeout()
+        }
         Image(painterResource(id = R.drawable.ic_crane_drawer), contentDescription = null)
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PreviewLadingScreen() {
+    LandingScreen {}
 }
