ab837b7bf36641bc80ab4346f0dfce1105131509
Oseong Kwon <oxojjjj@gmail.com>
2022-04-19 09:03:25 +0900

    Testing done

--- TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TobAppBarTest.kt
+++ TestingCodelab/app/src/androidTest/java/com/example/compose/rally/TobAppBarTest.kt
@@ -1,12 +1,13 @@
 package com.example.compose.rally
 
-import androidx.compose.ui.test.assertIsSelected
+import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.text.toUpperCase
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
 class TobAppBarTest {
 
@@ -26,8 +27,20 @@ class TobAppBarTest {
         }
         Thread.sleep(5000)
 
-        composeTestRule
+        /*composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+            .assertIsSelected()*/
+
+        composeTestRule.onRoot().printToLog("currentLabelExists")
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
     }
 }
